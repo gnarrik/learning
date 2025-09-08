3af755eaedd799a038f519ff42cbf2450cc48e5e
Glygalo Nikita <gnarrik@gmail.com>
2025-09-08 21:00:13 +0700

    Удален main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-const moveto = new MoveTo();
-const refs = document.querySelectorAll('a');
-const button = document.querySelector('button');
-refs.forEach(ref => moveto.registerTrigger(ref));
-moveto.registerTrigger(button);
