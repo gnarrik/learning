b2f226c5603e9535a107c1d362bf7e00187f4543
Nikita Glygalo <gnarrik@gmail.com>
2025-09-11 20:43:16 +0700

    отредактирована маска

--- mask.js
+++ mask.js
@@ -1,3 +1,17 @@
-IMask(document.querySelector('#registration-phone'), {
-	mask: '+{7}-(000)-000-00-00',
+document.addEventListener('DOMContentLoaded', function () {
+	// Применяем маску к полю с id registration-phone
+	const phoneInput = document.getElementById('registration-phone');
+	if (phoneInput) {
+		IMask(phoneInput, {
+			mask: '+{7}-(000)-000-00-00',
+		});
+	}
+
+	// Применяем маску к полям с классом registration-phone в модальном окне
+	const modalPhoneInputs = document.querySelectorAll('.registration-phone');
+	modalPhoneInputs.forEach(function (input) {
+		IMask(input, {
+			mask: '+{7}-(000)-000-00-00',
+		});
+	});
 });
